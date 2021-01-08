fcce31159bc63647946694519dfdb2ec929bd5a6
serge <serge@DESKTOP-24Q9VPV>
2021-01-08 18:34:56 -0500

    final

--- run_analyses.R
+++ run_analyses.R
@@ -1,30 +1,37 @@
 library(tidyverse)
 library(dplyr)
 library(purrr)
-library(tidyr)
+library(tidyr) 
 
-path_test<-"C:/Users/serge/Documents/UCI HAR Dataset/test/X_test.txt"
-path_train<-"C:/Users/serge/Documents/UCI HAR Dataset/train/X_train.txt"
-path_features<-"C:/Users/serge/Documents/UCI HAR Dataset/features.txt"
-res<-data.frame()
-d_train<-read.table(path_train)
-d_test<-read.table(path_test)
-d_tt<-rbind(d_train,d_test)
-d_features<-read.table(path_features)
-col_names<-d_features$V2
-d_tbl<-tbl_df(d_tt)
-colnames(d_tbl)<-col_names
-res<-d_tbl %>% select(contains("mean()"),contains("std()"))%>%
-  gather() %>%
-  mutate(sensor_signal = map_chr(key, ~unlist(strsplit(., "-"))[1])) %>%
-  mutate(mean_std = map_chr(key, ~unlist(strsplit(., "-"))[2])) %>%
-  mutate(xyz_axes = map_chr(key, ~unlist(strsplit(., "-"))[3])) %>%
-  mutate(xyz_axes = case_when(xyz_axes == "X" ~ "X",
-                              xyz_axes == "Y" ~ "Y",
-                              xyz_axes == "Z" ~ "Z",
-                              is.na(xyz_axes == NA) ~ "unknown")) %>%
-  select(sensor_signal, xyz_axes, mean_std, value) %>%
-  mutate_at(vars(sensor_signal, xyz_axes, mean_std), list(factor)) 
+  path_test<-"C:/Users/serge/Documents/UCI HAR Dataset/test/"
+  path_train<-"C:/Users/serge/Documents/UCI HAR Dataset/train/"
+  path_features<-"C:/Users/serge/Documents/UCI HAR Dataset/features.txt"
+  res<-data.frame()
+  d_train<-read.table(paste0(path_train,"X_train.txt"))
+  d_test<-read.table(paste0(path_test,"X_test.txt"))
+  activity_train<-read.table(paste0(path_train,"Y_train.doc.txt"))
+  activity_test<-read.table(paste0(path_test,"Y_test.txt"))
+  subject_train<-read.table(paste0(path_train,"subject_train.txt"))
+  subject_test<-read.table(paste0(path_test,"subject_test.txt"))
+  names(subject_test)[1]<-"subject"
+  names(subject_train)[1]<-"subject"
+  names(activity_train)[1]<-"activity"
+  names(activity_test)[1]<-"activity"
+  c_train<-cbind(d_train,activity_train,subject_train) 
+  c_test<-cbind(d_test,activity_test,subject_test)
+  d_tt<-rbind(c_train,c_test)
+  d_features<-read.table(path_features)
+  col_names<-c(d_features$V2, "activity","subject")
+  d_tbl<-tbl_df(d_tt)
+  colnames(d_tbl)<-col_names
+      res<-d_tbl %>% select("activity","subject",contains("mean()"),contains("std()"))%>%
+        mutate(activity=case_when(activity==1 ~ "WALKING",
+                                  activity==2 ~ "WALKING_UPSTAIRES",
+                                  activity==3 ~ "WALKING_DOWNSTAIRES",
+                                  activity==4 ~ "SITTING",
+                                  activity==5 ~ "STANDING",
+                                  activity==6 ~ "LAYING"))%>%
+      mutate_at(vars(activity, subject), list(factor))
   
-write.table(res, file="c:/Users/serge/Documents/res_tidy.txt",row.names = FALSE)
+write.table(avg_data, file="c:/Users/serge/Documents/res_tidy.txt",row.names = FALSE)
   
